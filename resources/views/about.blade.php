3eb2265b93d51a163c905db8faa7dd4e0230b8f7
Jerin1406 <79559253+Jerin1406@users.noreply.github.com>
2021-05-21 13:54:51 +0530

    D by c

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -232,7 +232,7 @@ Carmel Schools expansive and modern campus includes some of the finest facility.
           <!-- You can delete the links only if you purchased the pro version. -->
           <!-- Licensing information: https://bootstrapmade.com/license/ -->
           <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/ -->
-          Designed by <a href="#"></a>
+          Designed by <a href="#">Jerin K Thomas</a>
         </div>
       </div>
       <div class="social-links text-center text-md-right pt-3 pt-md-0">
